f9c392832db0f7b567812e728781a399e764f299
firebrunan <bruno_moia@hotmail.com>
2023-06-05 20:02:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System
+using System.Globalization
+
+    namespace Course
+{ 
+    public class Program
+    { 
+        public static void Main (string [] args)
+        {
+            Console.WriteLine("Digite um número entre 1 e 10" + )
+        }
+                
