46ce8b129782d13c1082bd131186cc4bd39081d2
firebrunan <bruno_moia@hotmail.com>
2023-06-05 22:14:49 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using System
-using System.Globalization
+﻿using System;
+using System.Globalization;
 
-    namespace Course
-{ 
+namespace Course
+{
     public class Program
-    { 
-        public static void Main (string [] args)
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Digite um número entre 1 e 10" + )
+
+            int num;
+
+            do
+            {
+                Console.Write("Digite um número entre 1 e 10: ");
+                num = int.Parse(Console.ReadLine());
+            } while (num < 1 || num > 10);
+            Console.WriteLine("Você digitou o número " + num + ".");
+
         }
-                
+    }
+}
