c5407705a8b5e81b497b0453f285b81077b74523
H1karu <124624556+H1kah@users.noreply.github.com>
2023-06-12 03:57:07 -0300

    Update Calculadora.cs

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Globalization;
-
+//como todo código, todo o arquivo precisa de uma biblioteca para que cada code seja compreendido, já que, sem eles, será apenas palavras sem signigicado nenhum.
 namespace Course;
-
+//é um recurso que ajuda a organizar e evitar conflitos de nomes em um código
 class Program {
+//para começar é necessário criar uma clase, no caso, está criando uma no public para que possa ser mencionada em outras partes do code.
     static void Main(string[] args) {
+//Não é mais int main, e sim, void main. Nessa parte, está criando outro public, citando o peso do comando(string), etc.
         char op;
         float a, b;
+        //aqui está citando as variáveis. O char e float seriam para variáveis que são letras, diferente de outros que são números
 
         Console.Write("Entre com o operador +, -, *, /: ");
-        op = Console.ReadKey().KeyChar;
-        Console.WriteLine();
-
+//para que resposda é necessário a pergunta. No caso, o Console.Write é o qeu aparecerá na tela
+        op = int.Parse(Console.ReadLine());
+//aqui o op é citado para que possa ser associado com qualquer resposta à seguir.
         Console.Write("Entre com dois valores: ");
-        string[] values = Console.ReadLine().Split(' ');
+//aqui é outra mensagem que aparecerá na tela, pedindo os valores desejados para fazer o cálculo
+        string[] values = Console.ReadLine().Split(' ');                                                            
         a = float.Parse(values[0]);
         b = float.Parse(values[1]);
 
